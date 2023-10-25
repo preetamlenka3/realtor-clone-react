a22f1a1df84d867098ca230eb46d0ac3cdbc49eb
P charan lenka <preetamlenka3@gmail.com>
2023-10-25 12:06:14 +0530

    create the create listing page UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
+import CreateListing from "./pages/CreateListing";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
       </Router>
       <ToastContainer
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import { getAuth, updateProfile } from "firebase/auth";
 import { doc, updateDoc } from "firebase/firestore";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
+import {FcHome} from "react-icons/fc";
 
 export default function Profile() {
   const auth = getAuth()
@@ -75,6 +76,12 @@ export default function Profile() {
             <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>Sign Out</p>
           </div>
         </form>
+        <button type="submit" className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">
+            <Link to="/create-listing" className="flex justify-center items-center">
+              <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2"/>
+              Sell or rent your home
+            </Link>
+          </button>
       </div>
     </section>
     </>
